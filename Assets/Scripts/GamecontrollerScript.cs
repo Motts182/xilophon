f94aff48a4c162134ac1843e66c4a7872767bc49
MZA\mdiaz <mnd.matias.diaz@gmail.com>
2017-03-01 17:31:25 -0300

    Develo mokup scens game and menu

---
+++ Assets/Scripts/GamecontrollerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamecontrollerScript : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
