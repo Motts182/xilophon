0c25e8c95ac477ca58a53f42283f065dffeb2693
Motts <mnd.matias.diaz@gmail.com>
2017-03-09 21:17:47 -0300

    Sprites and ITween

---
+++ Assets/solMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solMove : MonoBehaviour {
+    public GameObject lala;
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(lala, iTween.Hash("path", iTweenPath.GetPath("New Path 3"),"time",15f,"easetype",iTween.EaseType.easeInOutSine,"looptype","loop"));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
