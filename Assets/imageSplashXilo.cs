1d885b86d6f6165ceb4476d2c0653887c48944ff
MZA\mdiaz <mnd.matias.diaz@gmail.com>
2017-03-16 16:28:50 -0300

    press xilofono button and add for 5 sec imag of animal, onli need add real mp3

---
+++ Assets/imageSplashXilo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageSplashXilo : MonoBehaviour {
+    public Sprite trasnparente;
+    public Sprite animalito;
+
+	void Start () {
+        GetComponent<Image>().sprite = trasnparente;
+	}
+
+    public void ChagenGilado(Sprite animalitoAux) {
+        StartCoroutine(animalitoSprite(animalitoAux));
+
+    }
+
+    IEnumerator animalitoSprite(Sprite animalitoAux) {
+
+        GetComponent<Image>().sprite = animalitoAux;
+
+        yield return new WaitForSeconds(5);
+
+        GetComponent<Image>().sprite = trasnparente;
+
+        yield return null;
+    }
+}
