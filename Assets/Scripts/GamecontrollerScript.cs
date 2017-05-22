bc103f228185066746b228d290c1145c32f08ccb
MZA\mdiaz <mnd.matias.diaz@gmail.com>
2017-05-22 16:15:23 -0300

    Links and music Controller

--- Assets/Scripts/GamecontrollerScript.cs
+++ Assets/Scripts/GamecontrollerScript.cs
@@ -8,21 +8,22 @@ public class GamecontrollerScript : MonoBehaviour
     void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        GameObject[] gO = GameObject.FindGameObjectsWithTag("GameController");
+        if (gO.Length > 1) {
+            GameObject.Destroy(gO[0]);
+        }
     }
 
     public void ChangeLevel() {
         SceneManager.LoadScene(1);
+    }
+
+    public void MoreGames() {
+        Application.OpenURL("https://play.google.com/store/apps/developer?id=Bio-Raptor");
+    }
+
+    public void Like()
+    {
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.xilo.bio#details-reviews");
     }
 }
